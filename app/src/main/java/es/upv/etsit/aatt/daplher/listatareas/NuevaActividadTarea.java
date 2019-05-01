3461a1a26918a705a82124838d5382df6d08fd92
damian <daplher@teleco.upv.es>
2019-05-01 17:49:55 +0200

    creacion de backgrounds para los inputs

---
+++ app/src/main/java/es/upv/etsit/aatt/daplher/listatareas/NuevaActividadTarea.java
@@ -0,0 +1,13 @@
+package es.upv.etsit.aatt.daplher.listatareas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NuevaActividadTarea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nueva_actividad_tarea);
+    }
+}
