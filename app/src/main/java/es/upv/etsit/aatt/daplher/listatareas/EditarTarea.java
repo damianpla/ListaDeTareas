3ec5e0ac200b5e7831fd0a87aa4e86ff4a496390
damian <daplher@teleco.upv.es>
2019-05-01 20:59:27 +0200

    Funcion borrar implementada

--- app/src/main/java/es/upv/etsit/aatt/daplher/listatareas/EditarTarea.java
+++ app/src/main/java/es/upv/etsit/aatt/daplher/listatareas/EditarTarea.java
@@ -7,7 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -41,6 +44,23 @@ public class EditarTarea extends AppCompatActivity {
 
         reference = FirebaseDatabase.getInstance().getReference().child("CajaTareas").
                 child("Tareas" + keyidTareas);
+
+        btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reference.removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Intent a = new Intent(EditarTarea.this,MainActivity.class);
+                            startActivity(a);
+                        }else{
+                            Toast.makeText(getApplicationContext(),"¡Fail!",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
 
         // Funcionalidad de los botones
 
